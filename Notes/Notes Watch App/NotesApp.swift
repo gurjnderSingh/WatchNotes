e7952d2d4f75e7ea631f93a09a04a60f8b66b175
gurjnderSingh <gurjindersingh663@gmail.com>
2023-05-02 23:05:59 +0530

    initial commit

---
+++ Notes/Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Gurjinder Singh on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
